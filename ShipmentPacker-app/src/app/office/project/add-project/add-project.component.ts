ef111256645d37519dca312528d25787f6ad167b
Kristoffermc <kristoffermchristensen@gmail.com>
2017-11-23 14:16:52 +0100

    Created add-project for the office-guys

--- ShipmentPacker-app/src/app/office/project/add-project/add-project.component.ts
+++ ShipmentPacker-app/src/app/office/project/add-project/add-project.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+import {NgbDateStruct} from '@ng-bootstrap/ng-bootstrap';
+
+const now = new Date();
 
 @Component({
   selector: 'app-add-project',
@@ -7,9 +11,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddProjectComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  model: NgbDateStruct;
+  date: {year: number, month: number};
+
+  backbtn() {
+    this.router.navigateByUrl('/office');
+
+  }
 }
+
+
+
