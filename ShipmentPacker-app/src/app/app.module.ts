dd2916522107488558fc45535770103f5b215f9b
Kristoffermc <kristoffermchristensen@gmail.com>
2017-11-23 10:18:11 +0100

    Fixed a really early bug

--- ShipmentPacker-app/src/app/app.module.ts
+++ ShipmentPacker-app/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './utility/login/login.component';
-import { ProjectListComponent } from './office/project/project-list/project-list/project-list.component';
+import { ProjectListComponent } from './office/project/project-list/project-list.component';
 import { WorkshopProjectListComponent } from './workshop/project/workshop-project-list/workshop-project-list.component';
 
 
