47f5971b85f5c3dc1c2c8636ab814669a50c869e
jbitsme <jonas_husted@live.dk>
2017-12-01 12:36:32 +0100

    the edit function is done
    working

--- ShipmentPacker-app/src/app/office/project/edit-project/edit-project.component.ts
+++ ShipmentPacker-app/src/app/office/project/edit-project/edit-project.component.ts
@@ -3,6 +3,8 @@ import {ActivatedRoute, Router} from '@angular/router';
 import {NgbDateStruct} from '@ng-bootstrap/ng-bootstrap';
 import {Project} from '../shared/project.model';
 import {ProjectService} from '../shared/project.service';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'app-edit-project',
@@ -12,23 +14,41 @@ import {ProjectService} from '../shared/project.service';
 export class EditProjectComponent implements OnInit {
 
   project: Project;
+  projectGroup: FormGroup;
   constructor(private router: Router,
+              private fb: FormBuilder,
               private projectService: ProjectService,
-              private route: ActivatedRoute) { }
+              private route: ActivatedRoute){
+  this.projectGroup = this.fb.group({
+    projectName: ['', Validators.required],
+    customerName: ['', Validators.required],
+    creatorName: ['', Validators.required]
+  });
+  }
+
+  model: NgbDateStruct;
+  date: {year: number, month: number};
 
   ngOnInit() {
     this.route.paramMap.switchMap(params => this.projectService.getById(+params.get('id')))
       .subscribe(project => this.project = project);
   }
 
-  model: NgbDateStruct;
-  date: {year: number, month: number};
-
-  backbtn() {
+  back() {
     this.router.navigateByUrl('/office');
   }
 
   save() {
-    this.router.navigateByUrl('/office')
+    const values = this.projectGroup.value;
+    if (values.projectName == "") values.projectName = this.project.projectName;
+    if (values.customerName == "") values.customerName = this.project.customerName;
+    if (values.creatorName == "") values.creatorName = this.project.creatorName;
+    this.project = {
+      id: this.project.id,
+      projectName: values.projectName,
+      customerName: values.customerName,
+      creatorName: values.creatorName,
+    };
+    this.projectService.update(this.project).subscribe(proj => this.back());
   }
 }
