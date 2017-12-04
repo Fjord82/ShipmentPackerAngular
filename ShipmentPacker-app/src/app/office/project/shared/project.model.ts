ee7e6e84985bb0d9821caa20ed9c342386f8b78d
Jeppe Ehmsen <jeppeehmsen@gmail.com>
2017-12-04 15:32:45 +0100

    Can add packing lists to porjects

--- ShipmentPacker-app/src/app/office/project/shared/project.model.ts
+++ ShipmentPacker-app/src/app/office/project/shared/project.model.ts
@@ -1,7 +1,10 @@
+import {PackingList} from "../../../workshop/packingList/shared/packingList.model";
+
 export class Project {
   id?: number;
   projectName?: string;
   creatorName?: string;
   customerName?: string;
   isActive?: boolean;
+  packingLists?: PackingList[];
 }
