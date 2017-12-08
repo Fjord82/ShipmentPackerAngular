a1b7da8af83912f4474347a2e2af2e85713b6601
Kristoffermc <kristoffermchristensen@gmail.com>
2017-12-08 14:18:43 +0100

    Added inactive and active sorting and GUI fix

--- ShipmentPacker-app/src/app/workshop/packingList/workshop-packing-list/workshop-packing-list.component.ts
+++ ShipmentPacker-app/src/app/workshop/packingList/workshop-packing-list/workshop-packing-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {Packing} from '../../../office/packing/shared/packing.model';
 import {PackingService} from '../../../office/packing/shared/packing.service';
+import {UtilityService} from '../../../shared/utility.service';
 
 @Component({
   selector: 'app-workshop-project-list',
@@ -10,23 +11,30 @@ import {PackingService} from '../../../office/packing/shared/packing.service';
 })
 export class WorkshopPackingListComponent implements OnInit {
 
+  active: Packing[];
   packing: Packing;
   packings: Packing[];
   constructor(private router: Router,
               private packingService: PackingService,
-              private route: ActivatedRoute) { }
+              private route: ActivatedRoute,
+              private utilityService: UtilityService) { }
 
   ngOnInit() {
     this.packingService.getPackings().subscribe(
       packings => {
-        this.packings = packings;
+        this.sortLists(packings);
       });
+  }
+
+  sortLists(packing: Packing[]) {
+    this.active = this.utilityService.activeList(packing);
   }
 
 
   logoutbtn() {
     this.router.navigateByUrl('/login');
   }
+
   clickPackingDetail(packing: Packing) {
     this.router.navigateByUrl('/packingDetailWorkshop/'+packing.id)
   }
